6b8baa78ea58dd5857691d82b80311a4ba18fbe3
Julien Panopio <jcpanopio1@gmail.com>
2019-01-19 18:14:18 -0800

    Add login and token retrieval

---
+++ src/constants/endpoints.ts
@@ -0,0 +1,3 @@
+export const GITHUB_LOGIN_REDIRECT: String = 'https://github.com/login/oauth/authorize';
+
+export const GITHUB_ACCESS_TOKEN: String = 'https://github.com/login/oauth/access_token';
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,38 @@
 import path from 'path';
 import { Router } from 'express';
 import fetch from 'node-fetch';
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from './constants/env';
+import { GITHUB_LOGIN_REDIRECT, GITHUB_ACCESS_TOKEN } from './constants/endpoints';
 
 const router = Router();
 
 router.get('/', (_, res) => res.sendFile(path.join(__dirname, '../src/static/login.html')));
+
+router.get('/github-login', (_, res) => {
+  res.redirect(`${GITHUB_LOGIN_REDIRECT}?client_id=${GITHUB_CLIENT_ID}`);
+});
+
+router.get('/github-code', async (req) => {
+  const { query } = req;
+  const codeForAccessToken: String = query.code;
+  const response = await fetch(
+    GITHUB_ACCESS_TOKEN,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: GITHUB_CLIENT_ID,
+        client_secret: GITHUB_CLIENT_SECRET,
+        code: codeForAccessToken,
+      }),
+    },
+  );
+
+  const result = await response.json();
+  const accessToken: String = result.access_token;
+});
 
 export default router;
