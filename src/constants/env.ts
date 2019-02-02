73837bab70687055843a1803f70d403449bf886f
Julien Panopio <jcpanopio1@gmail.com>
2019-02-02 13:38:05 -0800

    Add env variable for session secret

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,2 +1,3 @@
 export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 export const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+export const APP_SESSION_SECRET = process.env.APP_SESSION_SECRET;
